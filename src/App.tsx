813904d6dad1c62c66a7b6e3166affbc96f796ce
Pola Rabiczko <pola@rabiczko.pl>
2026-01-20 20:57:38 +0100

    Add MilitaryOSMLayer component to the MapContainer

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,7 @@ export default function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MilitaryOSMLayer />
     </MapContainer>
   );
 }
